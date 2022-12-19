357d0ecd5bae3b192c981eaaf0497c2c0f62478f
Camilo Gonzalez <gscamilo22@gmail.com>
2022-12-19 22:20:47 +0100

    final

--- ExamenDIW2022/static/app.js
+++ ExamenDIW2022/static/app.js
@@ -1,3 +1,32 @@
-// nombre_del_alumno
+// Camilo González Sánchez
 //
 // Completar JS aqui
+
+// Modal
+var modal = document.getElementById("myModal");
+
+// Boton que abre el modal
+var btn = document.getElementById("myBtn");
+
+var wrapper = document.getElementById("main")
+// abrir el modal
+function crear() {
+//   modal.style.display = "block";
+  modal.style.visibility = "visible";
+  wrapper.style.visibility="hidden";
+}
+
+ cerrado = document.getElementsByID('#volver');
+//cierro el modal
+function cerrar() {
+    modal.style.visibility = "hidden";
+    wrapper.style.visibility="visible";
+  }
+  
+//Si esta el formulario relleno envia mensaje de datos enviados
+  function advertencia(){
+    var nombre = document.getElementById("nombre");
+    if(nombre.value!=""){
+    alert("Datos enviados con éxito");
+    }
+}
